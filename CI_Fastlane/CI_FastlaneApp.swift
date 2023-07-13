d05ac6d24775a11b8dd52c01680347170821d112
Thomas George <private@thomas-george.com>
2023-07-13 15:03:46 +0200

    Initial Commit

---
+++ CI_Fastlane/CI_FastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_FastlaneApp.swift
+//  CI_Fastlane
+//
+//  Created by Thomas George on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CI_FastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
